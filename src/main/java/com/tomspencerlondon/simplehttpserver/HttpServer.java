6510edf9dfea407e3ec2db0c8d6f6f5ca86d3ba7
Tom <thomas.spencer@codurance.com>
2022-07-25 00:12:51 +0100

    Add ConfigurationManager

--- src/main/java/com/tomspencerlondon/simplehttpserver/HttpServer.java
+++ src/main/java/com/tomspencerlondon/simplehttpserver/HttpServer.java
@@ -1,8 +1,19 @@
 package com.tomspencerlondon.simplehttpserver;
 
+import com.tomspencerlondon.simplehttpserver.config.Configuration;
+import com.tomspencerlondon.simplehttpserver.config.ConfigurationManager;
+import java.io.IOException;
+
 public class HttpServer {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello from HttpServer");
+
+    ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
+    Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+    System.out.println("Using port: " + conf.getPort());
+    System.out.println("Using webroot: " + conf.getWebroot());
   }
 }
--- src/main/java/com/tomspencerlondon/simplehttpserver/config/ConfigurationManager.java
+++ src/main/java/com/tomspencerlondon/simplehttpserver/config/ConfigurationManager.java
@@ -1,5 +1,12 @@
 package com.tomspencerlondon.simplehttpserver.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.tomspencerlondon.simplehttpserver.util.Json;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
 
   private static ConfigurationManager myConfigurationManager;
@@ -20,13 +27,48 @@ public class ConfigurationManager {
    *
    * Used to load a configuration file by the path provided
    */
-  public void loadConfigurationFile(String filePath) {
+  public void loadConfigurationFile(String filePath) throws IOException {
+    FileReader fileReader = null;
+
+    try {
+      fileReader = new FileReader(filePath);
+    } catch (FileNotFoundException e) {
+      throw new HttpConfigurationException(e);
+    }
+
+
+    StringBuffer sb = new StringBuffer();
+    int i;
+    try {
+      while ((i = fileReader.read()) != -1) {
+        sb.append((char) i);
+      }
+    } catch (IOException e) {
+      throw new HttpConfigurationException(e);
+    }
+
+    JsonNode conf = null;
+    try {
+      conf = Json.parse(sb.toString());
+    } catch (JsonProcessingException e) {
+      throw new HttpConfigurationException("Error parsing the Configuration file", e);
+    }
+
+    try {
+      myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+    } catch (JsonProcessingException e) {
+      throw new HttpConfigurationException("Error parsing the Configuration file, internal", e);
+    }
   }
 
   /**
    * returns the current loaded configuration
    */
-  public void getCurrentConfiguration() {
+  public Configuration getCurrentConfiguration() {
+    if (myCurrentConfiguration == null) {
+      throw new HttpConfigurationException("No Current Configuration Set.");
+    }
 
+    return myCurrentConfiguration;
   }
 }
